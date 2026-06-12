076480d5c938ceb719cf697a31f1a9684c2fef93
Nathan Spath <nathan.spath4@univ-lorraine.fr>
2026-06-12 09:21:58 +0200

    Interface pour les flous

---
+++ src/Flou.java
@@ -0,0 +1,10 @@
+public interface Flou {
+
+    /**
+     * Méthode pour le flou moyen
+     * @param r
+     */
+    public void flouMoyenne(int r);
+
+    public void flouGausien(int x, int y,double distribution);
+}
